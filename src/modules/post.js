3dc315c351e6d976aaf73528cec837a921e741e5
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2022-08-31 14:28:59 +0300

    Create a function to set data

---
+++ src/modules/post.js
@@ -0,0 +1,11 @@
+const user_name=document.querySelector(".name");
+const user_score=document.querySelector(".score");
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/wTWLt5kL1D7bko98XSeZ/scores/';
+// A function to setData.
+const setData=()=>{
+    await fetch(url,{
+        method: 'POST',
+        body: JSON.stringify({ user: user_name.value, score: user_score.value }),
+        headers: { 'Content-type': 'application/JSON' },
+    })
+}
